26686ef5226078e976de785385de99e025088696
Sujalsingh-digipine <sujal@digipineinfotech.com>
2025-07-15 18:26:30 +0530

    test commit

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -8,6 +8,7 @@ import {
 } from "../controllers/post.controller";
 import { authenticate } from "../middlewares/auth-user";
 
+// test comment
 export const postRouter = express.Router();
 postRouter.post("/", authenticate, createPostController);
 postRouter.get("/", getPostsController);
